32af55d04569ad0075a88a8f09748aef79299331
SaiTejaM12 <46732710+SaiTejaM12@users.noreply.github.com>
2019-01-21 18:38:01 -0600

    commit

--- newfils.js
+++ newfils.js
@@ -1,2 +1,24 @@
+var inA, inB;
 
-var 
+
+addEventListener(event:"click"{
+var   
+var displayNumber = displayNumber + click.textContent;
+}
+
+)
+
+function add(inA, inB) {
+  return (inA += inB);
+}
+function subtract(inA, inB) {
+  return (inA -= inB);
+}
+function divide(inA, inB) {
+  return (inA /= inB);
+}
+function multiply(inA, inB) {
+  return (inA *= inB);
+}
+
+
